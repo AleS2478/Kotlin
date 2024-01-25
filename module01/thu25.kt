7b4b7ae2f79c12b64205387cc699826ab205f08e
AleS2478 <157508194+AleS2478@users.noreply.github.com>
2024-01-25 11:11:36 -0600

    thu25.kt
    Saved old version

---
+++ module01/thu25.kt
@@ -0,0 +1,16 @@
+const val HERO_NAME = "Madrigal"
+
+fun main() {
+    //println("Hello, world!")
+    //println("My name is Alejandro, and I'm learning Kotlin")
+    println("The hero announces her presence to the World.")
+
+    println(HERO_NAME)
+    
+    var playerLevel = 4
+    println(playerLevel)
+
+    println("The hero embarks on her journey to locate the enchanted sword")
+    playerLevel += 1
+    println(playerLevel)
+}
