ba48f82242b753154d80f7aa0f694e985203fdcc
AleS2478 <157508194+AleS2478@users.noreply.github.com>
2024-02-01 09:56:34 -0600

    create a println code

---
+++ week04/println.kt
@@ -0,0 +1,10 @@
+/ HelloWorld/Task1.kt
+package helloWorldITSE1329
+
+// TODO
+
+fun main() {
+    println("Hello World!")
+    println("My name is Ignacio Alejandro")
+    println("I'm taking Kotlin in Spring 2024 Programming Logic and Design ITSE-1329-82G01")
+}
