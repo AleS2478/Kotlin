43f3030675f14d5c58f1328a903dd5eddd1f04aa
AleS2478 <157508194+AleS2478@users.noreply.github.com>
2024-02-13 11:09:29 -0600

    create salary.kt

---
+++ review/unit02/salary.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+
+    var name: String =  "Ignacio Alejandro"
+    var age: Int = 15
+    var annualPay: Double = 499446.6
+
+    println("My name is $name, my age is $age.")
+    println("I hope to earn $$annualPay per year.")
+}
