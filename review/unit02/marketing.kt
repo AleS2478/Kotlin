a2b25ec78cc5fd68e3e33f40d7ab8da4f65876df
AleS2478 <157508194+AleS2478@users.noreply.github.com>
2024-02-15 10:46:51 -0600

    create marketing.kt

---
+++ review/unit02/marketing.kt
@@ -0,0 +1,10 @@
+fun main()  {
+    val friendsCount = 280
+    if (friendsCount in 1..499) {
+        println("Buy Cat Food At 20% Off")
+    }
+
+    if (friendsCount >= 500) {
+        println("Adopt a Dog Today!")
+    }
+} 
